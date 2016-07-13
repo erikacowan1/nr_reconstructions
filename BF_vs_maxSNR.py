efdf94333fb8a31db8b9d59a7a2beb665b6542fa
erika.cowan <erikacowan1@gmail.com>
2016-07-13 18:28:05 -0400

    currently trying to loop over storing variables in single array. Working besides that

--- BF_vs_maxSNR.py
+++ BF_vs_maxSNR.py
@@ -14,21 +14,37 @@ args = parser.parse_args()
 bayes_factor_pattern = os.path.join(args.base_dir, 'posterior_samples','posterior_H1L1_*-{}.dat_B.txt')
 snr_pattern = os.path.join(args.base_dir, 'engine', 'lalinferencenest-{}-*.dat_snr.txt')
 
-
+BF_SNR_array = []
 for run in range(0, 12, 1):
 	wildcard_bayes_factor = bayes_factor_pattern.format(run)
 	wildcard_snr = snr_pattern.format(run)
 
-#	for filename in glob.glob(wildcard_snr)
-#		ratio = H1_snr / L1_snr
-#                max_SNR = max((ratio),(1/ratio))
-	bayesfactor = 0
 	for filename in glob.glob(wildcard_bayes_factor):
 		f = open(filename, 'r')
 		for line in f:
 			line = line.strip()
 			columns = line.split()
-			bayesfactor = columns[0]
+			bayesfactor = float(columns[0])
 		f.close()
+		#print bayesfactor
 
-	print bayesfactor
+	for filename in glob.glob(wildcard_snr):
+		f = open(filename, 'r')
+		H1, L1, network = f.readlines()	
+		
+		columns_H1 = H1.split()
+		columns_L1 = L1.split()
+		columns_network = network.split()
+
+		H1_snr = float(columns_H1[1])
+		L1_snr = float(columns_L1[1])
+		network_snr = float(columns_network[1])
+		
+		ratio = H1_snr / L1_snr
+		max_snr = max((ratio),(1/ratio))
+	#print H1_snr, L1_snr, network_snr, ratio, max_snr
+
+
+	BF_SNR_array.append(bayesfactor, H1_snr, L1_snr, network_snr, ratio, max_snr)
+
+print BF_SNR_array
